4a718896f86e73653b873a02f8ba832951a6df9d
Matth_eu <matthew@matth.eu>
2016-06-28 01:21:34 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp          = require( 'gulp' );
+const sourcemaps    = require( 'gulp-sourcemaps' );
+const watch         = require( 'gulp-watch' );
+const postcss       = require( 'gulp-postcss' );
+const autoprefixer  = require( 'autoprefixer' );
+const sass          = require( 'gulp-sass' );
+
+// Compile and minify CSS.
+gulp.task( 'styles', () => {
+	gulp.src( './src/styles/*.scss' )
+		.pipe( sourcemaps.init() )
+		.pipe( sass( { outputStyle: 'compressed' } )
+		.on( 'error', sass.logError ) )
+		.pipe( postcss( [ autoprefixer( { browsers: ['last 3 versions'] } ) ] ) )
+		.pipe( sourcemaps.write('.') )
+		.pipe( gulp.dest( './dist/styles' ) );
+});
+
+
+// Watch for changes in JS/CSS.
+gulp.task( 'watch', function() {
+	gulp.watch( 'src/styles/**/*.scss', ['styles'] );
+});
+
+// Tasks
+gulp.task( 'default', [ 'styles' ] );
