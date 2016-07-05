3bacc33a34c7a530ae542a0bf7df2d9dbd9fb6f1
Matth_eu <matthew@matth.eu>
2016-07-05 17:41:06 +0100

    Use Vanilla JS for search

--- src/js/SearchBar.js
+++ src/js/SearchBar.js
@@ -1,22 +1,33 @@
 (function() {
 
-	$('.SearchBar').each( function() {
+	var searchBars = document.getElementsByClassName( 'SearchBar' );
 
-		var $searchBar = $(this);
-		var $field     = $( '.SearchBar_Field', $searchBar );
+	for ( var i = 0; i < searchBars.length; i++ ) {
 
-		$field.focus( function() {
-			$searchBar.addClass( 'SearchBar-Focused' );
+		var searchBar = searchBars[ i ];
+		var field     = searchBar.getElementsByClassName( 'SearchBar_Field' );
+
+		if ( field.length < 1 ) {
+			return;
+		} else {
+			field = field[0];
+		}
+
+		field.addEventListener( "focus", function() {
+			searchBar.classList.add( 'SearchBar-Focused' );
 		} );
 
-		$field.blur( function() {
-			$searchBar.removeClass( 'SearchBar-Focused' );
+		field.addEventListener( "blur", function() {
+			searchBar.classList.remove( 'SearchBar-Focused' );
 		} );
 
-		$field.keyup( function() {
-			$searchBar.toggleClass( 'SearchBar-HasResults', $field.val().length > 0 );
+		field.addEventListener( "keyup", function() {
+			if ( field.value.length > 0 ) {
+				searchBar.classList.add( 'SearchBar-HasResults' );
+			} else {
+				searchBar.classList.remove( 'SearchBar-HasResults' );
+			}
 		} );
+	}
 
-	} );
-
-})(jQuery);
+})();
