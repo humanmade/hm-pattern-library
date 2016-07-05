585848c7d55fe3dd6768b817f3bdcada353a424b
Matth_eu <matthew@matth.eu>
2016-07-05 19:39:36 +0100

    First pass at no jQuery

--- src/js/NavAccordion.js
+++ src/js/NavAccordion.js
@@ -1,54 +1,76 @@
-(function($) {
+(function() {
 
-	var navAccordion = function( $el ) {
+	var navAccordion = function( el ) {
 
-		$el.children().each( function() {
+		var children = el.children;
 
-			var $NavAccordion_Item = $(this);
-			var $subNav = $NavAccordion_Item.children( 'ul' );
 
-			if ( $subNav.length > 0 ) {
+		for ( var i = 0; i < children.length; i++ ) {
 
-				var openHeight           = $subNav.outerHeight();
-				var $NavAccordion_Toggle = $( '<button/>', {
-					text: 'Toggle',
-					class: 'Btn NavAccordion_Toggle'
-				} );
+			var navAccordion_Item = children[i];
 
-				var toggleSubNav = function( show ) {
+			var subNav = Array.prototype.filter.call( navAccordion_Item.children, function( el ) {
+				return el.tagName === 'UL';
+			} );
 
-					if ( 'undefined' === typeof( show ) ) {
-						show = $NavAccordion_Item.hasClass( 'NavAccordion_Item-Closed' );
-					}
-
-					if ( show ) {
-						$NavAccordion_Item.removeClass( 'NavAccordion_Item-Closed' );
-						$NavAccordion_Item.addClass( 'NavAccordion_Item-Open' );
-						$subNav.css( 'max-height', openHeight );
-					} else {
-						$NavAccordion_Item.addClass( 'NavAccordion_Item-Closed' );
-						$NavAccordion_Item.removeClass( 'NavAccordion_Item-Open' );
-						$subNav.css( 'max-height', 0 );
-					}
-
-				}
-
-				$NavAccordion_Toggle.appendTo( $NavAccordion_Item.children('.NavAccordion_Anchor') );
-				toggleSubNav( false );
-
-				$NavAccordion_Toggle.click( function( e ) {
-					e.preventDefault();
-					$(this).blur();
-					toggleSubNav();
-				});
+			if ( subNav.length < 1 ) {
+				continue;
 			}
 
-		} );
+			var openHeight = subNav.offsetHeight;
+			var style      = getComputedStyle( subNav );
+
+			openHeight += parseInt( style.marginTop ) + parseInt( style.marginBottom );
+
+			var navAccordion_Toggle = document.createElement( 'BUTTON' );
+			navAccordion_Toggle.appendChild( document.createTextNode( "Toggle" ) );, {
+			navAccordion_Toggle.classList.add( 'Btn NavAccordion_Toggle' );
+
+		}
+		// 	if ( $subNav.length > 0 ) {
+
+		// 		var openHeight           = $subNav.outerHeight();
+		// 		var $NavAccordion_Toggle = $( '<button/>', {
+		// 			text: 'Toggle',
+		// 			class: 'Btn NavAccordion_Toggle'
+		// 		} );
+
+		// 		var toggleSubNav = function( show ) {
+
+		// 			if ( 'undefined' === typeof( show ) ) {
+		// 				show = $NavAccordion_Item.hasClass( 'NavAccordion_Item-Closed' );
+		// 			}
+
+		// 			if ( show ) {
+		// 				$NavAccordion_Item.removeClass( 'NavAccordion_Item-Closed' );
+		// 				$NavAccordion_Item.addClass( 'NavAccordion_Item-Open' );
+		// 				$subNav.css( 'max-height', openHeight );
+		// 			} else {
+		// 				$NavAccordion_Item.addClass( 'NavAccordion_Item-Closed' );
+		// 				$NavAccordion_Item.removeClass( 'NavAccordion_Item-Open' );
+		// 				$subNav.css( 'max-height', 0 );
+		// 			}
+
+		// 		}
+
+		// 		$NavAccordion_Toggle.appendTo( $NavAccordion_Item.children('.NavAccordion_Anchor') );
+		// 		toggleSubNav( false );
+
+		// 		$NavAccordion_Toggle.click( function( e ) {
+		// 			e.preventDefault();
+		// 			$(this).blur();
+		// 			toggleSubNav();
+		// 		});
+		// 	}
+
+		// } );
 
 	}
 
-	$('.NavAccordion').each( function() {
-		navAccordion( $(this) );
-	} );
+	var els = document.getElementsByClassName('NavAccordion');
 
-} )(jQuery);
+	for ( var i = 0; i < els.length; i++ ) {
+		navAccordion( els[ i ] );
+	}
+
+} )();
