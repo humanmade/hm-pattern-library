eaadd7269b64f95d5dfe570f1372356a5c0ba3bc
Matth_eu <matthew@matth.eu>
2016-07-11 15:39:49 +0100

    Update - collapse nested nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,77 +9,74 @@
 		return;
 	}
 
-	var navAccordion = function( el ) {
+	var initNavAccordion = function( navAccordionItem ) {
 
-		Array.prototype.forEach.call( el.children, function( navAccordionItem ) {
-
-			var subNav = Array.prototype.filter.call( navAccordionItem.children, function( el ) {
-				return el.tagName === 'UL';
-			} );
-
-			if ( subNav.length < 1 ) {
-				return;
-			} else {
-				subNav = subNav[0];
-			}
-
-			var openHeight = subNav.offsetHeight;
-			var style      = getComputedStyle( subNav );
-
-			openHeight += parseInt( style.marginTop ) + parseInt( style.marginBottom );
-
-			var navAccordionToggle = document.createElement( 'BUTTON' );
-			navAccordionToggle.appendChild( document.createTextNode( "Toggle" ) );
-			navAccordionToggle.classList.add( 'Btn' );
-			navAccordionToggle.classList.add( 'NavAccordion_Toggle' );
-
-			var anchor = Array.prototype.filter.call( navAccordionItem.children, function( el ) {
-				return el.tagName === 'A';
-			} );
-
-			if ( anchor.length ) {
-				anchor[0].appendChild( navAccordionToggle );
-			}
-
-			var toggleSubNav = function( show ) {
-
-				if ( 'undefined' === typeof show ) {
-					show = navAccordionItem.classList.contains( 'NavAccordion_Item-Closed' );
-				}
-
-				if ( show ) {
-					navAccordionItem.classList.remove( 'NavAccordion_Item-Closed' );
-					navAccordionItem.classList.add( 'NavAccordion_Item-Open' );
-					subNav.style.maxHeight = openHeight + 'px';
-				} else {
-					navAccordionItem.classList.add( 'NavAccordion_Item-Closed' );
-					navAccordionItem.classList.remove( 'NavAccordion_Item-Open' );
-					subNav.style.maxHeight = 0;
-				}
-
-			}
-
-			if ( subNav.getElementsByClassName( 'NavAccordion_Item-Active' ).length > 0 ) {
-				toggleSubNav( true );
-			} else {
-				toggleSubNav( false );
-			}
-
-			navAccordionToggle.addEventListener( 'click', function( event ) {
-				event.preventDefault();
-				navAccordionToggle.blur();
-				toggleSubNav();
-			});
-
+		var subNav = Array.prototype.filter.call( navAccordionItem.children, function( el ) {
+			return el.tagName === 'UL';
 		} );
 
+		if ( subNav.length < 1 ) {
+			return;
+		} else {
+			subNav = subNav[0];
+		}
+
+		var openHeight = subNav.offsetHeight;
+		var style      = getComputedStyle( subNav );
+
+		openHeight += parseInt( style.marginTop ) + parseInt( style.marginBottom );
+
+		var navAccordionToggle = document.createElement( 'BUTTON' );
+		navAccordionToggle.appendChild( document.createTextNode( "Toggle" ) );
+		navAccordionToggle.classList.add( 'Btn' );
+		navAccordionToggle.classList.add( 'NavAccordion_Toggle' );
+
+		var anchor = Array.prototype.filter.call( navAccordionItem.children, function( el ) {
+			return el.tagName === 'A';
+		} );
+
+		if ( anchor.length ) {
+			anchor[0].appendChild( navAccordionToggle );
+		}
+
+		var toggleSubNav = function( show ) {
+
+			if ( 'undefined' === typeof show ) {
+				show = navAccordionItem.classList.contains( 'NavAccordion_Item-Closed' );
+			}
+
+			if ( show ) {
+				navAccordionItem.classList.remove( 'NavAccordion_Item-Closed' );
+				navAccordionItem.classList.add( 'NavAccordion_Item-Open' );
+				navAccordionToggle.classList.add( 'NavAccordion_Toggle-Open' );
+				subNav.style.maxHeight = openHeight + 'px';
+			} else {
+				navAccordionItem.classList.add( 'NavAccordion_Item-Closed' );
+				navAccordionItem.classList.remove( 'NavAccordion_Item-Open' );
+				navAccordionToggle.classList.remove( 'NavAccordion_Toggle-Open' );
+				subNav.style.maxHeight = 0;
+			}
+
+		}
+
+		if ( subNav.getElementsByClassName( 'NavAccordion_Item-Active' ).length > 0 ) {
+			toggleSubNav( true );
+		} else {
+			toggleSubNav( false );
+		}
+
+		navAccordionToggle.addEventListener( 'click', function( event ) {
+			event.preventDefault();
+			navAccordionToggle.blur();
+			toggleSubNav();
+		});
 	}
 
-	var els = document.getElementsByClassName('NavAccordion');
-
-	for ( var i = 0; i < els.length; i++ ) {
-		navAccordion( els[ i ] );
-	}
+	// Init Accordion Nav for all NavAccordion Items.
+	Array.prototype.forEach.call(
+		document.getElementsByClassName( 'NavAccordion_Item' ),
+		initNavAccordion
+	);
 
 } )( window );
 
