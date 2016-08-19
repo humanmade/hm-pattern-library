c019caad64fa037296a9ea097be18c6e3ecbee49
Matth_eu <matthew@matth.eu>
2016-08-19 15:48:24 +0100

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,12 +49,12 @@
 				navAccordionItem.classList.remove( 'NavAccordion_Item-Closed' );
 				navAccordionItem.classList.add( 'NavAccordion_Item-Open' );
 				navAccordionToggle.classList.add( 'NavAccordion_Toggle-Open' );
-				subNav.style.maxHeight = openHeight + 'px';
+				subNav.style.height = 'auto';
 			} else {
 				navAccordionItem.classList.add( 'NavAccordion_Item-Closed' );
 				navAccordionItem.classList.remove( 'NavAccordion_Item-Open' );
 				navAccordionToggle.classList.remove( 'NavAccordion_Toggle-Open' );
-				subNav.style.maxHeight = 0;
+				subNav.style.height = 0;
 			}
 
 		}
